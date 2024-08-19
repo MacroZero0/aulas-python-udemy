23d91616efb657dfe4d2053c3be9638eafae2afd
MacroZero0 <ebarreto375@gmail.com>
2024-08-19 06:54:25 -0300

    renamed file for organization

--- aula17.py
+++ aula17.py
@@ -1,11 +1,20 @@
 # if / elif      / else
 # se / se não se / se não
-condicao - True
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
 
-if condicao:
-    print('Este é o código do if')
+if condicao1:
+    print('Código para condição 1') # condição falsa então não será executado
+elif condicao2:
+    print('Código para condição 2') # condição falsa então não será executado
+elif condicao3:
+    print('Código para condição 3') # condição Verdadeira então será executado
+elif condicao4:
+    print('Código para condição 4') # condição falsa então não será executado
 else:
-    print('')
+    print('Nenhuma condição foi satisfeita') # se nenhum if por executado, else será exec.
 
 if 10 == 10:
     print('Outro if')
